ef95f991f548e61b2cfa83e505a74528206a6737
david_wesh <davidwaweru2500@gmail.com>
2024-12-23 01:32:35 +0300

    Register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,14 @@ export const LoginSchema = z.object({
     })
 });
 
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "please provide a valid email"
+    }),
+    password: z.string().min(8, {
+        message: "Minumum of 8 characters required"
+    }),
+    name: z.string().min(1, {
+        message: "Name is required",
+    })
+});
