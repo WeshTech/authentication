1a03920b173b830f8bec0d19fde335bb00488efb
david_wesh <davidwaweru2500@gmail.com>
2024-12-22 23:55:49 +0300

    login form

---
+++ schemas/index.ts
@@ -0,0 +1,11 @@
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+    email: z.string().email({
+        message: "please provide a valid email"
+    }),
+    password: z.string().min(1, {
+        message: "password is required"
+    })
+});
+
